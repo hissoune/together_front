c31574f0e07d970484d11a0c206caf8a1e6df8a0
hissoune <khalidhissoune962@gmail.com>
2024-11-21 16:04:54 +0100

    Add : adding the api url to controle th axios

---
+++ src/api/Api.ts
@@ -0,0 +1,9 @@
+import {Config} from "../helpers/ConfigHelper";
+import axios from "axios";
+
+const Api = () => {
+    const token = localStorage.getItem("token");
+    return axios.create(Config(token));
+}
+
+export default Api
