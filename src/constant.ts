520c47dd6d83fd2ea97dc18fa490684a1f90dd39
hissoune <khalidhissoune962@gmail.com>
2024-11-21 16:05:21 +0100

    Add : adding the constances

---
+++ src/constant.ts
@@ -0,0 +1,47 @@
+export interface MenuItem {
+    title: string;
+    icon: JSX.Element;
+    gap?: boolean;
+    onClick?: () => void;
+}
+
+export interface ConfigOptions {
+    baseURL: string;
+    headers: {
+        'Content-Type': string;
+        'Authorization': string;
+    };
+}
+
+export interface NotificationDropdownProps {
+    isOpen: boolean;
+    toggleDropdown: () => void;
+}
+
+export interface FriendsProps {
+    onFriendClick: () => void;
+}
+
+export interface Contacts{
+    image: string | null;
+    name: string;
+    message: string;
+    time: number
+    isOnline: boolean;
+}
+
+export interface RegisterField{
+    name: string;
+    email: string;
+    password: string;
+    phone: number;
+}
+
+export interface LoginField{
+    email: string;
+    password: string;
+}
+
+export interface FriendField{
+    name: string;
+}
