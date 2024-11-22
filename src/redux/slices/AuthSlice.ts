67f2ad1624f6254b6aaf790bca32cee99e7e5920
hissoune <khalidhissoune962@gmail.com>
2024-11-22 05:38:11 +0100

    Add :adding the auth slice in slices in redux folder

---
+++ src/redux/slices/AuthSlice.ts
@@ -0,0 +1,78 @@
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import AuthService from "../../services/AuthService.ts";
+import { initialState } from "../initialisation.ts";
+
+export const register = createAsyncThunk(
+    "auth/register",
+    async ({ name, email, password }: { name: string; email: string; password: string }, { rejectWithValue }) => {
+        try {
+            const response = await AuthService.register({ name, email, password });
+            return response.data;
+        } catch (error) {
+            return rejectWithValue(error.response?.data || error.message);
+        }
+    }
+);
+
+export const login = createAsyncThunk(
+    "auth/login",
+    async (
+        { email, password }: { email: string; password: string },
+        { rejectWithValue }
+    ) => {
+        try {
+            const response = await AuthService.login({ email, password });
+            console.log('hello', response)
+            return response.data;
+        } catch (error: any) {
+            const errorMessage = error.response?.data || error.message || "Something went wrong.";
+            return rejectWithValue(errorMessage);
+        }
+    }
+);
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        changeStateTrue: (state) => {
+            state.updateState = true;
+        },
+        changeStateFalse: (state) => {
+            state.updateState = false;
+        },
+        clearResponse: (state) => {
+            state.response = "";
+        },
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(register.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(register.fulfilled, (state, action: PayloadAction<any>) => {
+                state.loading = false;
+                state.dataObj = action.payload;
+                state.errorMessage = null;
+            })
+            .addCase(register.rejected, (state, action: PayloadAction<string | undefined>) => {
+                state.loading = false;
+                state.errorMessage = action.payload || "Registration failed";
+            })
+            .addCase(login.pending, (state) => {
+                state.loading = true;
+            })
+            .addCase(login.fulfilled, (state, action: PayloadAction<any>) => {
+                state.loading = false;
+                state.dataObj = action.payload;
+                state.errorMessage = null;
+            })
+            .addCase(login.rejected, (state, action: PayloadAction<string | undefined>) => {
+                state.loading = false;
+                state.errorMessage = action.payload || "login failed";
+            })
+
+    }
+})
+
+export default authSlice.reducer;
