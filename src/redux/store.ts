ff4c92d8e30454367b5f754fdde3d50583a2003c
hissoune <khalidhissoune962@gmail.com>
2024-11-22 20:47:29 +0100

    Add : adding the reducer of the auth to the store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,10 @@
 import {configureStore} from "@reduxjs/toolkit";
+import authSlice from './slices/AuthSlice';
 
 
 export const store = configureStore({
     reducer: {
-       
+       auth:authSlice
     }
 })
 export type RootState = ReturnType<typeof store.getState>;
